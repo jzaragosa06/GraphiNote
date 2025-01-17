dd49981661b83c613fa0f0eff7fadb7a2b16cd6e
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-01-17 22:33:24 +0800

    feat: add session

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
+require("dotenv").config();
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
+const session = require("express-session");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//configuration of session middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) =>
+{
     res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
-app.get("/lab", (req, res) => {
+app.get("/lab", (req, res) =>
+{
     res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.listen(3000, () => {
+app.listen(3000, () =>
+{
     console.log("listening on port 3000");
 
-})
+});
